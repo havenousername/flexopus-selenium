e3e344bfae4a3b2ca156c8cfb2501e3a62cc3f7b
andreicristea <andrei.cristea@apicore.de>
2022-05-10 23:46:17 +0200

    fix: random data and deprecated test comment out

--- src/test/java/UserBookingProcessTest.java
+++ src/test/java/UserBookingProcessTest.java
@@ -1,6 +1,10 @@
 import org.junit.Test;
 import pages.BookingProcessTestPages;
 import utils.BookableType;
+import utils.Calendars;
+
+import java.util.Date;
+import java.util.Random;
 
 public class UserBookingProcessTest extends BasicTest {
     private BookingProcessTestPages bookingProcessTestPages;
@@ -14,14 +18,28 @@ public class UserBookingProcessTest extends BasicTest {
 
     @Test
     public void testBookableTypeProcesses() {
-       testBookableTypeProcesses(BookableType.WORKING_SPACE);
-       testBookableTypeProcesses(BookableType.PARKING_SPOT);
-       testBookableTypeProcesses(BookableType.MEETING_ROOM);
+        testBookableTypeProcessesRand(BookableType.WORKING_SPACE);
+        testBookableTypeProcessesRand(BookableType.PARKING_SPOT);
+        // deprecated in latest release
+        // testBookableTypeProcessesRand(BookableType.MEETING_ROOM);
     }
 
     private void testBookableTypeProcesses(BookableType bookableType) {
         bookingProcessTestPages.goToSelectBookDate(bookableType);
         bookingProcessTestPages.selectBookDate(11);
+        bookingProcessTestPages.selectBookTimeFrom(3);
+        bookingProcessTestPages.selectBookTimeTo(6);
+        bookingProcessTestPages.searchBookablesInTimeRange(bookableType);
+        bookingProcessTestPages.selectLocation(1);
+        bookingProcessTestPages.hoverAndSelectBookable(1, bookableType);
+        bookingProcessTestPages.goToSelectBookDate();
+    }
+
+    private void testBookableTypeProcessesRand(BookableType bookableType) {
+        bookingProcessTestPages.goToSelectBookDate(bookableType);
+        Random rand = new Random();
+        int day = rand.nextInt(Calendars.lastMonthDay());
+        bookingProcessTestPages.selectBookDate(day);
         bookingProcessTestPages.selectBookTimeFrom(3);
         bookingProcessTestPages.selectBookTimeTo(6);
         bookingProcessTestPages.searchBookablesInTimeRange(bookableType);
--- src/test/java/pages/BookingProcessTestPages.java
+++ src/test/java/pages/BookingProcessTestPages.java
@@ -2,7 +2,6 @@ package pages;
 
 import org.junit.Assert;
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import utils.ApplyCookies;
@@ -12,18 +11,16 @@ import utils.LocalStorageManipulations;
 
 import java.io.UnsupportedEncodingException;
 import java.time.Duration;
-import java.time.temporal.TemporalUnit;
-import java.util.concurrent.TimeUnit;
 
 public class BookingProcessTestPages extends GoBackPageBase {
     private final By workStationSelect = By.xpath(
             "//*[contains(@class, \"screen-card\")]//*[contains(@class, \"selector-container\")]//div[1]/div[1]/div[2]/button"
     );
     private final By meetingRoomsSelect = By.xpath(
-            "//*[contains(@class, \"screen-card\")]//*[contains(@class, \"selector-container\")]//div[2]/div[1]/div[2]/button"
+            "//*[contains(@class, \"screen-card\")]//*[contains(@class, \"selector-container\")]//div[3]/div[1]/div[2]/button"
     );
     private final By parkingSelect = By.xpath(
-            "//*[contains(@class, \"screen-card\")]//*[contains(@class, \"selector-container\")]//div[3]/div[1]/div[2]/button"
+            "//*[contains(@class, \"screen-card\")]//*[contains(@class, \"selector-container\")]//div[2]/div[1]/div[2]/button"
     );
 
     private final By selectFromSelector = By.xpath(
@@ -74,6 +71,12 @@ public class BookingProcessTestPages extends GoBackPageBase {
         final By daySelector = By.xpath(
                 "//div[contains(@class, 'vc-day') and contains(@class, '" + dayClass +"')]"
         );
+        try {
+            driver.findElement(daySelector).findElement(By.xpath("//span[contains(@class, 'is-disabled')]"));
+        } catch (NoSuchElementException elementException) {
+            selectBookDate(day + 1);
+            return;
+        }
         WebElement selectDay = waitVisibilityAndFindElement(daySelector);
         selectDay.click();
     }
@@ -120,7 +123,7 @@ public class BookingProcessTestPages extends GoBackPageBase {
         var objectDriver = driver.switchTo().frame(waitVisibilityAndFindElement(By.id("floorplan")));
         WebElement selectedBookable = waitVisibilityAndFindElement(objectDriver, By.xpath("//*[@id='" + bookableType.getName() +"']//svg:g[" + select +"]"));
         Actions actions = new Actions(objectDriver);
-        actions.moveToElement(selectedBookable).clickAndHold().build().perform();
+        actions.moveToElement(selectedBookable).perform();
         String fill = selectedBookable.findElement(By.tagName("path")).getCssValue("fill");
         String rgbColor = "rgb(255, 255, 255)";
         Assert.assertNotEquals("Values are not different. Not Excepted" + rgbColor + ", got " + fill + " at element " + selectedBookable.getAttribute("id"),rgbColor, fill);
